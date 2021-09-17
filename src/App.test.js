7ac242527be9cc6d678a40b73e778a9179fd7df9
Matt Elliott <eliotmat@gmail.com>
2021-09-17 15:59:40 -0400

    bruh build this back bettah

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/HELLO NERDZZZZ11!!1/i);
+  const linkElement = screen.getByText(/HELLO BRUH/i);
   expect(linkElement).toBeInTheDocument();
 });
